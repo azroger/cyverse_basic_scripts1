c11cc092024ea7c7e8326821710b232d9870213b
Roger Barthelson <rogerab@email.arizona.edu>
2016-05-17 14:44:23 -0700

    Update Sam2SortedBam.sh
    Lower memory setting to avoid failure.

--- Sam2SortedBam.sh
+++ Sam2SortedBam.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 samtools view -bS $1 $2 > temp.bam
-samtools sort $3 -m 3000000000 temp.bam sorted
+samtools sort $3 -m 2000000000 temp.bam sorted
 if [[ $3 == '' ]] ;
 then
 samtools index sorted.bam
