854fcc6616e412dfde5ba5edf5a84b037f4a89d1
Roger Barthelson <rogerab@email.arizona.edu>
2016-05-17 13:50:46 -0700

    Modify Sam2SortedBam.sh
    Add a conditional statement to the indexing step, because it fails when the sorting is name-based.

--- Sam2SortedBam.sh
+++ Sam2SortedBam.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-
 samtools view -bS $1 $2 > temp.bam
-samtools sort $3 -m 2000000000 temp.bam sorted
+samtools sort $3 -m 3000000000 temp.bam sorted
+if [[ $3 == '' ]] ;
+then
 samtools index sorted.bam
-
+fi
 rm temp.bam
+
