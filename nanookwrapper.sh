c98aa210be7d2f147422b07fd84a55022bc472a3
Roger Barthelson <rogerab@email.arizona.edu>
2016-08-03 09:50:34 -0700

    Fix syntax for nanook usage
    The first script was based on running with a docker image for Nanook. This script is now properly designed (hopefully) to run within the docker container for use by the Discovery Environment.

--- nanookwrapper.sh
+++ nanookwrapper.sh
@@ -2,13 +2,13 @@
 
 alignment=$3
 
-docker run -v $(pwd):/work -w /work nano-ok:0.0.1 extract -q -f /work/$1 -s /work
+nanook extract -q -f /work/$1 -s /work
 
 if [ -n "$alignment" ];
 then
 #  align (with premade index — using full fasta name for prefix) with bwa
-docker run -v $(pwd):/work -w /work nano-ok:0.0.1 align -aligner bwa  -r /work/$2 -s /work
+nanook align -aligner bwa  -r /work/$2 -s /work
 fi
 
-docker run -v $(pwd):/work -w /work nano-ok:0.0.1 analyze -aligner bwa  -r /work/$2 -s /work 
+nanook analyze -aligner bwa  -r /work/$2 -s /work 
 
